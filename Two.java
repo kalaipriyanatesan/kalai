facfa494736a4236d9628676e7013c8923155241
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-09-07 17:31:14 +0530

    Create Two.java

---
+++ Two.java
@@ -0,0 +1,16 @@
+public class Two {
+
+	public static void main(String[] args) {
+		int a = -1;
+		if (a == 0) {
+			System.out.println("zero");
+		} else if (a>0)
+
+		{
+			System.out.println("POSITIVE");
+		} else
+			System.out.println("NEGATIVE");
+
+	}
+
+}
