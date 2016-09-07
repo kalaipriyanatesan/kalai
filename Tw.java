66646ab8dd3bc95f670344b0cebf3cb2c482ad1e
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-09-07 17:15:52 +0530

    Create Tw.java

---
+++ Tw.java
@@ -0,0 +1,16 @@
+public class Tw {
+
+	public static void main(String[] args) {
+		int a = 0;
+		if (a == 0) {
+			System.out.println("zero");
+		} else if (a % 2 == 0)
+
+		{
+			System.out.println("even");
+		} else
+			System.out.println("odd");
+
+	}
+
+}
