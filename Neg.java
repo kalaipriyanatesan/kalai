c312288464cd64f3abd07eb0ba9d641a17ca0c96
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-09-07 17:27:01 +0530

    Update and rename Tw.java to Neg.java

---
+++ Neg.java
@@ -0,0 +1,16 @@
+public class Neg {
+
+	public static void main(String[] args) {
+		int a = -1;
+		if (a == 0) {
+			System.out.println("zero");
+		} else if (a>0)
+
+		{
+			System.out.println("POSITIVE");
+		} else
+			System.out.println("NEGATIVE");
+
+	}
+
+}
