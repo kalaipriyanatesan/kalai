9a9a7ce07456a6efc21c6f9bbaffd0c346962ec9
kalaipriyanatesan <kalaipriya.natesan@gmail.com>
2016-09-07 17:35:32 +0530

    Create Negative.java

---
+++ Negative.java
@@ -0,0 +1,20 @@
+
+public class Negative {
+
+	public static void main(String[] args) {
+		int a = -1;
+		if (a == 0) {
+			System.out.println("zero");
+		} else if (a>0)
+
+		{
+			System.out.println("POSITIVE");
+		} else
+			System.out.println("NEGATIVE");
+
+	}
+
+
+	}
+
+
